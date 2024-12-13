a7ea135bc687540d4ea13c8936f4257761412dea
VaibhavNB <badigervaibhav8@gmail.com>
2024-12-14 00:00:33 +0530

    feat: Add pagination and filtering to getAllVideos controller and all the video.controller.js file work done Pend: Local files to be removed

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,47 +1,205 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Video} from "../models/video.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { Video } from "../models/video.model.js";
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
-})
+  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
+
+  let filter = {};
+
+  if (query) {
+    filter = {
+      $or: [
+        { title: { $regex: query, $options: "i" } },
+        { description: { $regex: query, $options: "i" } },
+      ],
+    };
+  }
+
+  if (userId) {
+    filter.owner = userId;
+  }
+
+  let sort = {};
+  if (sortBy && sortType) {
+    sort[sortBy] = sortType === "asc" ? 1 : -1;
+  } else {
+    sort.createdAt = -1; // default sort by creation date in descending order
+  }
+
+  const videos = await Video.find(filter)
+    .sort(sort)
+    .skip((page - 1) * limit)
+    .limit(limit);
+
+  const count = await Video.countDocuments(filter);
+
+  res.status(200).json(
+    new ApiResponse(200, {
+      videos,
+      pagination: {
+        page,
+        limit,
+        totalPages: Math.ceil(count / limit),
+        totalCount: count,
+      },
+    })
+  );
+});
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    const { title, description} = req.body
-    // TODO: get video, upload to cloudinary, create video
-})
+  const { title, description } = req.body;
+
+  // const videoLocalPath = req.file?.video[0].path
+  let videoLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.videoFile) &&
+    req.files.videoFile.length > 0
+  ) {
+    videoLocalPath = req.files.videoFile[0].path;
+  }
+  // const thumbnailLocalPath = req.file?.thumbnail[0].path
+  let thumbnailLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.thumbnail) &&
+    req.files.thumbnail.length > 0
+  ) {
+    thumbnailLocalPath = req.files.thumbnail[0].path;
+  }
+
+  if (!videoLocalPath) {
+    throw new ApiError(400, "Please upload a video");
+  }
+
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "Please upload a thumbnail");
+  }
+
+  // upload them to cloudinary
+  const video = await uploadOnCloudinary(videoLocalPath);
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+  if (!video) {
+    throw new ApiError(400, "Error uploading video");
+  }
+
+  if (!thumbnail) {
+    throw new ApiError(400, "Error uploading thumbnail");
+  }
+
+  // create video object
+  const videoObj = await Video.create({
+    title,
+    description,
+    videoFile: video.url,
+    thumbnail: thumbnail.url,
+    duration: video.duration,
+    owner: req.user._id,
+  });
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, videoObj, "Video published successfully"));
+
+  // TODO: get video, upload to cloudinary, create video
+});
 
 const getVideoById = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: get video by id
-})
+  const { videoId } = req.params;
+  //TODO: get video by id
+  const video = await Video.findById(videoId);
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video fetched successfully"));
+});
 
 const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: update video details like title, description, thumbnail
+  const { videoId } = req.params;
+  //TODO: update video details like title, description, thumbnail
+  const { title, description } = req.body;
+  const thumbnailLocalPath = req.file?.path;
 
-})
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "Please upload a thumbnail");
+  }
+
+  // upload them to cloudinary
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+  if (!thumbnail) {
+    throw new ApiError(400, "Error uploading thumbnail");
+  }
+
+  // create video object
+  const videoObj = await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: {
+        title,
+        description,
+        thumbnail: thumbnail.url,
+      },
+    },
+    { new: true }
+  );
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, videoObj, "Video updated successfully"));
+});
 
 const deleteVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    //TODO: delete video
-})
+  const { videoId } = req.params;
+  //TODO: delete video
+  const video = await Video.findByIdAndDelete(videoId);
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video deleted successfully"));
+});
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-})
+  const { videoId } = req.params;
+  //TODO: toggle publish status
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  const isPublishedCheck = video.isPublished;
+  await Video.findByIdAndUpdate(
+    videoId,
+    {
+      $set: {
+        isPublished: !isPublishedCheck,
+      },
+    },
+    { new: true }
+  );
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        isPublishedCheck,
+        "Video published status toggled successfully"
+      )
+    );
+});
 
 export {
-    getAllVideos,
-    publishAVideo,
-    getVideoById,
-    updateVideo,
-    deleteVideo,
-    togglePublishStatus
-}
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+};
